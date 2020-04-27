b6f03e937faffeb9523e2755c48269b43b60a8f8
Ahmad Musa <ahmad@cloudify.co>
2020-04-27 16:34:09 +0300

    Terraform-Docker-Example

--- variables.tf
+++ variables.tf
@@ -1,31 +1,23 @@
+variable "key_name" {
+  description = "A name for the key you are importing."
+}
+
+variable "public_key" {
+  description = <<DESCRIPTION
+Public Key Material.
+DESCRIPTION
+}
+
 variable "aws_region" {
-  type = string
   description = "AWS region to launch servers."
 }
 
-variable "admin_user" {
-  type = string
-  description = "Admin user for the AMI we're launching"
-}
-
-variable "admin_key_public" {
-  type = string
-  description = "Public SSH key of admin user"
-}
-
-variable "access_key" {
-  type = string
-  description = "Access key for AWS"
-}
-
-variable "secret_key" {
-  type = string
-  description = "Secret key for AWS"
-}
-
+# CentOS 7.7.1908 x86_64 with cloud-init (HVM)
 variable "aws_amis" {
-  type = map(string)
   default = {
-    ca-central-1 = "ami-033e6106180a626d0"
+    eu-west-1 = "ami-0eee6eb870dc1cefa"
+    us-east-1 = "ami-03248a0341eadb1f1"
+    us-west-1 = "ami-01dd5a8ef26e6341d"
+    us-west-2 = "ami-024b56adf74074ca6"
   }
 }
